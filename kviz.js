6e9d65791f64e7bec985782bcbaec411254c1163
Adamyx <cseh.adam@kreativszolnok.hu>
2025-11-27 10:47:28 +0100

    Kerdesek

---
+++ kviz.js
@@ -0,0 +1,21 @@
+const feladatok = [
+    {
+        subject: "matematika",
+        question: "Mi a háromszög kerületének képlete?",
+        answers: ["K = a + b + c", "K = a * b * c", "T = a + b + c", "T = a * b * c"],
+        correct: 0
+    },
+    {
+        subject: "történelem",
+        question: "Mikor volt a forradalom?",
+        answers: ["1848.aug.21", "1849.marc.15", "1849.feb.31", "1848.marc.15"],
+        correct: 3
+    },
+    {
+        subject: "magyar irodalom",
+        question: "Mennyi színből áll Az ember tragédiája?",
+        answers: ["10", "15", "18", "12"],
+        correct: 1
+    }
+
+];
